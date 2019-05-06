81b6544935ed060d48bafa29fbaed737e26cedbb
Uta Celeste Urushido <utaurushido@Utas-MacBook-Pro.local>
2019-05-06 13:34:52 +0200

    Splash screen w a cute ass logo

---
+++ PlantOrDieApp/app/src/main/java/hu/ait/plantordieapp/SplashActivity.kt
@@ -0,0 +1,23 @@
+package hu.ait.plantordieapp
+
+import android.app.Activity
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.Menu
+
+class SplashActivity : Activity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.splash_file)
+
+            Handler().postDelayed({
+                    val intent = Intent(this, MainActivity::class.java);
+                    startActivity(intent);
+                    finish();
+            },3000)
+    }
+}
