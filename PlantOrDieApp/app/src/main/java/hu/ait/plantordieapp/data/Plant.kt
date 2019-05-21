df264a101a769f3ce2fa1a3faa6f0a6af2c22bd8
Silvie Coheleach <silviec98@gmail.com>
2019-05-21 17:17:10 +0200

    edited plant object to have appropriate parameters, added plant info activity

--- PlantOrDieApp/app/src/main/java/hu/ait/plantordieapp/data/Plant.kt
+++ PlantOrDieApp/app/src/main/java/hu/ait/plantordieapp/data/Plant.kt
@@ -3,12 +3,15 @@ package hu.ait.plantordieapp.data
 import android.arch.persistence.room.ColumnInfo
 import android.arch.persistence.room.Entity
 import android.arch.persistence.room.PrimaryKey
+import java.io.FileDescriptor
 import java.io.Serializable
 
 @Entity(tableName = "plant")
 data class Plant(
     @PrimaryKey(autoGenerate = true) var plantId : Long?,
-    @ColumnInfo(name = "createDate") var createDate: String,
-    @ColumnInfo(name = "done") var done: Boolean,
-    @ColumnInfo(name = "plantText") var plantText: String
+    @ColumnInfo(name = "plantNickname") var nickname: String,
+    @ColumnInfo(name = "plant") var plant: String,
+    @ColumnInfo(name = "plantText") var description: String,
+    //@ColumnInfo(name = "daysOwned") var daysOwned: String,
+    @ColumnInfo(name = "wateredToday") var wateredToday: Boolean
     ) : Serializable
